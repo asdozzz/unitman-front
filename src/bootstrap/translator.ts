337046ee7c9e9d180c06d26781e1bf2cd8057f0b
asdozzzz <asdozzz89@gmail.com>
2025-07-02 00:48:04 +0500

    unitman-front-35 Вывести сообщение в случае отсу-я полей в конфиге

--- src/bootstrap/translator.ts
+++ src/bootstrap/translator.ts
@@ -3,6 +3,7 @@ import ru from '@/translator/ru.json';
 import en from '@/translator/en.json';
 
 const i18n = createI18n({
+    allowComposition: true,
     locale: import.meta.env.VITE_DEFAULT_LOCALE || "ru",
     fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE || "en",
     legacy: false,
