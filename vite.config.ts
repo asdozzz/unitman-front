990e30c14619d59b1c7e59910925ccfbfadd5131
asdozzzz <asdozzz89@gmail.com>
2025-07-01 23:59:00 +0500

    Revert "Revert "unitman-front-27 Реализовать красивый вывод в логах задач раннера""
    This reverts commit 4ed3759d69fa7dc21646cfffa4fbced538ef9959.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'url';
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
-import vueI18n from '@intlify/vite-plugin-vue-i18n';
 import { resolve, dirname } from 'node:path';
 
 // https://vitejs.dev/config/
@@ -14,10 +13,6 @@ export default defineConfig({
   plugins: [
     vue({
       template: { transformAssetUrls },
-    }),
-    vueI18n({
-      // you need to set i18n resource including paths
-      include: resolve(dirname(fileURLToPath(import.meta.url)), './src/translator'),
     }),
     quasar(),
   ],
